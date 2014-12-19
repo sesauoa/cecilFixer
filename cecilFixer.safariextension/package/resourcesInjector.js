02cc4319714ba280d4f9cdf66837ef0cf2dd4bcc
Saren Currie <saren@sarencurrie.com>
2014-12-19 13:39:51 +1300

    Removed unnescesary files from .safariextension/package

--- cecilFixer.safariextension/package/resourcesInjector.js
+++
@@ -1,9 +0,0 @@
-function loadInternal(tag, name){
-	var tag = document.createElement(tag);
-	tag.src = chrome.extension.getURL(name);
-	return tag;
-}
-
-
-$('head').prepend(loadInternal('script', 'jquery-1.7.2.min.js'));
-$('head').prepend(loadInternal('script', 'resourcesFixer.js'));
