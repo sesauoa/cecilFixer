48ec089436ce174c216777bb3903cf4cd1641736
Saren Currie <saren@sarencurrie.com>
2015-02-04 19:15:00 +1300

    Created build script, eliminating the need for duplicate files.

---
+++ safariBuild.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Sets up cecilFixer.safariExtension for building by copying the js files into said directory
+
+cp ./package/jquery-1.7.2.min.js ./cecilFixer.safariextension/package
+cp ./package/popupFixer.js ./cecilFixer.safariextension/package
+cp ./package/resourcesFixer.js ./cecilFixer.safariextension/package
