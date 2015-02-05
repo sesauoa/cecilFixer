da15a9de55612a61aa257de482c542b5729b8a97
Saren Currie <saren@sarencurrie.com>
2015-02-06 10:12:18 +1300

    Improved build script so that duplicate files can be gitignored.

--- safariBuild.sh
+++ safariBuild.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # Sets up cecilFixer.safariExtension for building by copying the js files into said directory
 
-cp ./package/jquery-1.7.2.min.js ./cecilFixer.safariextension/package
-cp ./package/popupFixer.js ./cecilFixer.safariextension/package
-cp ./package/resourcesFixer.js ./cecilFixer.safariextension/package
+mkdir ./cecilFixer.safariextension/package/duplicates
+
+cp ./package/jquery-1.7.2.min.js ./cecilFixer.safariextension/package/duplicates/
+cp ./package/popupFixer.js ./cecilFixer.safariextension/package/duplicates/
+cp ./package/resourcesFixer.js ./cecilFixer.safariextension/package/duplicates/
